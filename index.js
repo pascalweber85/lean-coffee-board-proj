4c17de48938584b148d1dd3121ac299a06fed111
Pascal Weber <weber-pascal@web.de>
2021-05-27 15:05:33 +0200

    add "code"

---
+++ index.js
@@ -0,0 +1,18 @@
+const MongoClient = require('mongodb').MongoClient
+const asser = require('assert')
+
+const url = 'mongodb//localhost:27017'
+
+const dbName = 'lean-coffee-board'
+
+MongoClient.connect(url, function (err, client) {
+  assert.strictEqual(null, err)
+  console.log('Connected correctly to server')
+
+  const db = client.db(dbName)
+  const collection = db.collection('users')
+
+  collection.insertOne({ name: 'Passi', age: 36, email: 'PW@gmail.com' })
+
+  client.close()
+})
